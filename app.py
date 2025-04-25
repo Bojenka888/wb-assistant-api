75a262acbb2dea5a4715cb1423cc8b189e89d86e
Bojenka888 <evlobova@gmail.com>
2025-04-25 18:46:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def search():
 def home():
     return "Wildberries ассистент работает!"
     if __name__ == "__main__":
-    import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        import os
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
